1e5b30f91c68dea1914151f0e2a153e1981b59a2
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2025-10-23 22:48:57 +0300

    added comments for code

--- src/task-04/helpers/add_contact.py
+++ src/task-04/helpers/add_contact.py
@@ -3,7 +3,7 @@ from decorators.input_error import input_error
 @input_error
 def add_contact(args, contacts):
     if len(args) != 2:
-        raise IndexError
+        raise ValueError
       
     name, phone = args
     contacts[name] = phone
