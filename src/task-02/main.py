e84814e135257d8386b66c794747c07c4ed143dc
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2025-10-25 10:25:17 +0300

    formatting code, updataded helpers

--- src/task-02/main.py
+++ src/task-02/main.py
@@ -1,12 +1,16 @@
+"""
+Module for generating numbers from text and calculating their sum.
+"""
+
 from typing import Callable, Generator
 import re
 
 
-def generator_numbers(text: str)->Generator[float, None, None]:
+def generator_numbers(text: str) -> Generator[float, None, None]:
     """Number generator from text"""
 
     # Regular expression for numbers
-    regex = r'\b\d+(?:\.\d+)?\b'
+    regex = r"\s(\d+(?:\.\d+)?)\s"
 
     for string_number in re.findall(regex, text):
         try:
@@ -16,17 +20,19 @@ def generator_numbers(text: str)->Generator[float, None, None]:
             # skip any invalid values
             continue
 
+
 def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]):
     """Sums all numbers from text using the func generator."""
     return sum(func(text))
 
-if __name__ == '__main__':
-    text = """
-    Загальний дохід працівника складається 
-    з декількох частин: 1000.01 як основний 
-    дохід, доповнений додатковими надходженнями 
+
+if __name__ == "__main__":
+    input_text = """
+    Загальний дохід працівника складається
+    з декількох частин: 1000.01 як основний
+    дохід, доповнений додатковими надходженнями
     27.45 і 324.00 доларів.
     """
 
-    total_income = sum_profit(text, generator_numbers)
+    total_income = sum_profit(input_text, generator_numbers)
     print(f"Загальний дохід: {total_income}")
